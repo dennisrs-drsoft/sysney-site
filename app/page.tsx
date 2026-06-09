ed1104a26d332b172dc34cb82fb8dbb44c5cef67
Dennis Rodrigues <dennisrs@gmail.com>
2026-06-09 11:39:50 -0300

    Primeira página SYSNEY

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <main>
-      <h1>SYSNEY</h1>
-      <p>SISBlink - Plataforma B2B para Moda</p>
+    <main style={{ padding: "40px" }}>
+      <h1>SYSNEY Informática</h1>
+      <p>SISBlink - Plataforma B2B para gestão comercial de moda.</p>
     </main>
   );
 }
